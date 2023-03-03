73c20f340689ef67cc2ec5a4ef625f4794a82e09
CH V N G Samba Siva Reddy <ssrchinta@gmail.com>
2023-03-04 02:46:45 +0530

    theme changed

--- src/components/BlogComponent.js
+++ src/components/BlogComponent.js
@@ -5,22 +5,25 @@ import styles from "../styles/blog.module.css";
 const BlogComponent = (props) => {
     return (
         <div className={styles["blog-comp__wrapper"]}>
-            <div className={styles["blog-comp__about"]}>
-                <h3>{`${props.id}. ${props.title}`}</h3>
-                <p>{props.date}</p>
+            <div className={styles['blog-comp__highlighter']}>
+                <h1>{props.title[0].toUpperCase()}</h1>
             </div>
-            <div className={styles["blog-comp__details"]}>
-                <div>
-                    {props.domains.map((domain) => {
-                        return <span key={Math.random()}>{domain}</span>;
-                    })}
+            <div>
+                <div className={styles["blog-comp__meta_wrapper"]}>
+                    <div className={styles["blog-comp__meta"]}>
+                        <h3>{props.title}</h3>
+                        <p>{props.date}</p>
+                    </div>
+                    <button className={styles['open__btn']} onClick={() => alert("Not yet defined!") }>Open</button>
                 </div>
-                <button
-                    className={styles["open_btn"]}
-                    onClick={() => alert("Functionality not added. Thank You!")}
-                >
-                    Open
-                </button>
+                <div className={styles["blog-comp__details"]}>
+                    <div className={styles['blog-description']}>{props.description}</div>
+                    <div>
+                        {props.domains.map((domain) => {
+                            return <span key={Math.random()}>{domain}</span>;
+                        })}
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
+import HomeIcon from "@mui/icons-material/Home";
 import BlogComponent from "../components/BlogComponent";
 import styles from "../styles/blog.module.css";
 
@@ -10,31 +10,42 @@ const DUMMY_BLOGS = [
         title: "Variables in JS",
         domains: ["javascript"],
         date: "March 02 2023",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
     },
     {
         title: "Accessing DOM Elements",
         domains: ["javascript", "html"],
         date: "March 01 2023",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
     },
 ];
 
 const Blog = () => {
-    let counter = 1;
     return (
         <div className={styles["blog-wrapper"]}>
             <div className={styles["blog-main"]}>
-                <Link to="/" className={styles["home-link"]} fontSize="small">
-                    <KeyboardBackspaceIcon /> Home
-                </Link>
+                <button className={styles["home-btn"]}>
+                    <Link
+                        to="/"
+                        className={styles["home-link"]}
+                        fontSize="small"
+                    >
+                        Return to
+                    </Link>
+                    <HomeIcon />
+                </button>
                 <div className={styles["blogs"]}>
+                <form className={styles['search-form']}>
+                    <input type="search" placeholder="Start typing to get the blog you are looking..." />
+                </form>
                     {DUMMY_BLOGS.map((blog) => {
                         return (
                             <BlogComponent
                                 key={Math.random()}
-                                id={counter++}
                                 title={blog.title}
                                 domains={blog.domains}
                                 date={blog.date}
+                                description={blog.description}
                             />
                         );
                     })}
