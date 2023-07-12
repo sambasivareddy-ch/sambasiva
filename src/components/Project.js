40be40d0ecfe5d41301254868db1a155343f3ca5
CH V N G Samba Siva Reddy <ssrchinta@gmail.com>
2023-07-12 22:34:24 +0530

    status styles changed

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,20 +4,22 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import styles from "../styles/page.module.css";
 
 const Project = (props) => {
-    const classes = `${styles["pro-wrapper"]} ${props.className}`
+    const classes = `${styles["pro-wrapper"]} ${props.className}`;
     return (
         <div className={classes}>
             <div className={styles["project-header"]}>
                 <FolderIcon />
-                <div>
-                    <span className={"status"}>{ props.project_details.status }</span>
-                <a
-                    href={props.project_details.gitlink}
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <GitHubIcon fontSize="small"/>
-                </a>
+                <div className={styles["header-meta"]}>
+                    <span className={`${styles["status"]} ${styles[props.className]}`}>
+                        {props.project_details.status}
+                    </span>
+                    <a
+                        href={props.project_details.gitlink}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <GitHubIcon fontSize="medium" />
+                    </a>
                 </div>
             </div>
             <div className={styles["project-body"]}>
@@ -26,9 +28,9 @@ const Project = (props) => {
             </div>
             <div className={styles["project-footer"]}>
                 <ul>
-                {props.project_details.skills.map((skill) => {
-                    return <li key={Math.random()}>{skill}</li>;
-                })}
+                    {props.project_details.skills.map((skill) => {
+                        return <li key={Math.random()}>{skill}</li>;
+                    })}
                 </ul>
             </div>
         </div>
