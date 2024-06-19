96b2bfcf88a20674b33fb3c3d775a1cdd245d928
Samba Siva Reddy <151616423+sambasivareddy-ch@users.noreply.github.com>
2024-06-19 17:28:35 +0000

    Vercel SpeedInsights Added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import { Route, Routes } from 'react-router-dom';
 import ProjectPage from './pages/ProjectPage';
 import Main from './pages/Main';
 
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 const App = () => {
   return (
-      <Routes>
-        <Route path='/' element={<Main />} exact />
-        <Route path='/projects' element={<ProjectPage />} exact />
-      </Routes>
+      <>
+        <Routes>
+          <Route path='/' element={<Main />} exact />
+          <Route path='/projects' element={<ProjectPage />} exact />
+        </Routes>
+        <SpeedInsights/>
+      </>
   );
 }
 
