56252dc8ae1098407812e8b76fb5f9db5f790da5
Samba Siva Reddy <151616423+sambasivareddy-ch@users.noreply.github.com>
2024-07-03 07:20:49 +0000

    CSS Refactor & Add drive link for Resume

--- src/components/About.js
+++ src/components/About.js
@@ -4,38 +4,38 @@ import styles from "../styles/page.module.css";
 import samba from "../assests/samba.jpg";
 
 const About = () => {
-    return (
-        <div className={styles["about-div__wrapper"]} id="about">
-            <img src={samba} alt="Samba" className={styles["my-img"]} />
-            <div className={styles["about-wrapper"]}>
-                <h2 className={styles["title"]}>About Me</h2>
-                <p className={styles["about-me"]}>
-                    Hello There! I'm V N G Samba Siva Reddy Chinta. Currenly, I
-                    am working as 'Member Technical Staff' in Zoho Corporation, Chennai.
-                    And I also have Computer Science undergraduate degree from
-                    GVPCE (2019 - 2023) with CGPA 9.05.
-                </p>
-                <p className={styles["my-skills"]}>
-                    Here are the skills and technologies I'm working on:
-                </p>
-                <ul className={styles["skills-list"]}>
-                    <li>HTML, CSS</li>
-                    <li>JavaScript (ES6)</li>
-                    <li>React.js</li>
-                    <li>Express.js</li>
-                    <li>Node.js</li>
-                    <li>Docker</li>
-                    <li>Kubernetes</li>
-                    <li>Python</li>
-                    <li>Java</li>
-                    <li>C, C++</li>
-                    <li>MongoDB</li>
-                    <li>postgreSQL</li>
-                    <li>Go language</li>
-                </ul>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles["about-div__wrapper"]} id="about">
+      <img src={samba} alt="Samba" className={styles["my-img"]} />
+      <div className={styles["about-wrapper"]}>
+        <h2 className={styles["title"]}>About Me</h2>
+        <p className={styles["about-me"]}>
+          Hello There! I'm V N G Samba Siva Reddy Chinta. Currenly, I am working
+          as <span className={styles["highlight"]}>Member Technical Staff</span>{" "}
+          in Zoho Corporation, Chennai. And I also have Computer Science
+          undergraduate degree from GVPCE (2019 - 2023) with CGPA 9.05.
+        </p>
+        <p className={styles["my-skills"]}>
+          Here are the skills and technologies I'm working on:
+        </p>
+        <ul className={styles["skills-list"]}>
+          <li>HTML, CSS</li>
+          <li>JavaScript (ES6)</li>
+          <li>React.js</li>
+          <li>Express.js</li>
+          <li>Node.js</li>
+          <li>Docker</li>
+          <li>Kubernetes</li>
+          <li>Python</li>
+          <li>Java</li>
+          <li>C, C++</li>
+          <li>MongoDB</li>
+          <li>postgreSQL</li>
+          <li>Go language</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,26 +4,31 @@ import styles from "../styles/page.module.css";
 import resume from "../assests/samba_resume.pdf";
 
 const Intro = () => {
-    return (
-        <div className={styles["intro-wrapper"]}>
-            <p className={styles["welcome-msg"]}>Hi, my name is</p>
-            <h1 className={styles["my-name"]}>Samba Siva Reddy</h1>
-            <p className={styles["intro-me"]}>
-                I'm working as{" "}
-                <span className={styles["designation"]}>Software Developer (MTS)</span>{" "}
-                in Zoho Corporation. I am a Full Stack Web Developer based on MERN stack. I am currently working on building CLI Applications & Web servers using Go.
-                <br />
-            </p>
-            <a
-                href={resume}
-                alt="resume"
-                download={true}
-                className={styles["resume-link"]}
-            >
-                Resume
-            </a>
-        </div>
-    );
+  return (
+    <div className={styles["intro-wrapper"]}>
+      <p className={styles["welcome-msg"]}>Hi, my name is</p>
+      <h1 className={styles["my-name"]}>Samba Siva Reddy</h1>
+      <p className={styles["intro-me"]}>
+        I'm working as{" "}
+        <span className={styles["highlight"]}>Software Developer (MTS)</span> in
+        Zoho Corporation. I am a{" "}
+        <span className={styles["dashed-highlight"]}>
+          Full Stack Web Developer
+        </span>{" "}
+        based on MERN stack. I am currently working on building CLI Applications
+        & Web servers using Go.
+        <br />
+      </p>
+      <a
+        href="https://drive.google.com/file/d/1NBU-rR74c9QTxOnF5u5cRcAZNQYeIBpS/view?usp=drive_link"
+        alt="resume"
+        className={styles["resume-link"]}
+        target="_blank"
+      >
+        Resume
+      </a>
+    </div>
+  );
 };
 
 export default Intro;
