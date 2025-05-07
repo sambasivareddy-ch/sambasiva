289a7840697ab49e060eecdc79eaff798a772563
V N G Samba Siva Reddy Chinta <151616423+sambasivareddy-ch@users.noreply.github.com>
2025-05-07 05:22:41 +0000

    Dark Mode added & UI changed

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,20 +7,70 @@ const Skills = (props) => {
     <div className={styles["skills-wrapper"]} id="skills">
       <h2 className={styles["title"]}>Skills</h2>
       <ol className={styles["skills-list"]}>
-        <li className={styles["skill"]}>C language</li>
-        <li className={styles["skill"]}>C++</li>
-        <li className={styles["skill"]}>Java</li>
-        <li className={styles["skill"]}>Python</li>
-        <li className={styles["skill"]}>JavaScript</li>
-        <li className={styles["skill"]}>HTML5</li>
-        <li className={styles["skill"]}>CSS</li>
-        <li className={styles["skill"]}>React.js</li>
-        <li className={styles["skill"]}>Express.js</li>
-        <li className={styles["skill"]}>Node.js</li>
-        <li className={styles["skill"]}>GoLang</li>
-        <li className={styles["skill"]}>Docker</li>
-        <li className={styles["skill"]}>PostgreSQL</li>
-        <li className={styles["skill"]}>MongoDB</li>
+        <li className={styles["skill"]}>
+          <img src={"https://skillicons.dev/icons?i=nodejs"} alt="node.js"/>
+          Node.js
+        </li>
+        <li className={styles["skill"]}>
+          <img src={"https://skillicons.dev/icons?i=expressjs"} alt="express.js"/>
+          Express.js
+        </li>
+        <li className={styles["skill"]}>
+          <img src={"https://skillicons.dev/icons?i=react"} alt="react"/>
+          React
+        </li>
+        <li className={styles["skill"]}>
+          <img src={"https://skillicons.dev/icons?i=typescript"} alt="typescript"/>
+          Typescript
+        </li>
+        <li className={styles["skill"]}>
+          <img src={"https://skillicons.dev/icons?i=docker"} alt="docker"/>
+          Docker
+        </li>
+        <li className={styles["skill"]}>
+          <img src={"https://skillicons.dev/icons?i=kubernetes"} alt="kubernetes"/>
+          Kubernetes
+        </li>
+        <li className={styles["skill"]}>
+          <img src={"https://skillicons.dev/icons?i=javascript"} alt="javascript"/>
+          Javascript
+        </li>
+        <li className={styles["skill"]}>
+          <img src={"https://skillicons.dev/icons?i=go"} alt="go"/>
+          Go Lang
+        </li>
+        <li className={styles["skill"]}>
+          <img src={"https://skillicons.dev/icons?i=python"} alt="python3"/>
+          Python
+        </li>
+        <li className={styles["skill"]}>
+          <img src={"https://skillicons.dev/icons?i=java"} alt="java"/>
+          Java
+        </li>
+        <li className={styles["skill"]}>
+          <img src={"https://skillicons.dev/icons?i=html"} alt="html"/>
+          HTML5
+        </li>
+        <li className={styles["skill"]}>
+          <img src={"https://skillicons.dev/icons?i=css"} alt="css"/>
+          CSS3
+        </li>
+        <li className={styles["skill"]}>
+          <img src={"https://skillicons.dev/icons?i=postgres"} alt="postgres"/>
+          Postgres
+        </li>
+        <li className={styles["skill"]}>
+          <img src={"https://skillicons.dev/icons?i=mongodb"} alt="mongodb"/>
+          MongoDB
+        </li>
+        <li className={styles["skill"]}>
+          <img src={"https://skillicons.dev/icons?i=git"} alt="git"/>
+          Git
+        </li>
+        <li className={styles["skill"]}>
+          <img src={"https://skillicons.dev/icons?i=figma"} alt="figma"/>
+          Figma
+        </li>
       </ol>
     </div>
   );
