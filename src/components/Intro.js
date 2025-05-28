c11b6b769173597247489e6b06b80a441a35e01f
V N G Samba Siva Reddy Chinta <151616423+sambasivareddy-ch@users.noreply.github.com>
2025-05-28 12:20:37 +0000

    Styling changed

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -9,9 +9,9 @@ const Intro = () => {
   return (
     <div className={styles["intro-wrapper"]}>
       <p className={styles["welcome-msg"]}>
-        <h3 className={styles["wishes"]}>Namaste! I'm <b className={styles['my-name']}>Samba Siva Reddy</b>.</h3> 
+        <h3 className={styles["wishes"]}>Namaste! I'm <b className={styles['my-name']}>Samba Siva Reddy</b></h3> 
       </p>
-      <h1 className={styles["my-role"]}><DataObjectIcon/> Software Developer</h1>
+      <h1 className={styles["my-role"]}> Software Developer</h1>
       <p><b>I build scalable applications for Web & Mobile.</b></p>
       <p className={styles["intro-me"]}>
         <p className={styles["about-me"]}>
