5c7e271714d7c47fe50ce0c044a818d9072497cd
V N G Samba Siva Reddy Chinta <151616423+sambasivareddy-ch@users.noreply.github.com>
2025-06-06 04:51:19 +0000

    Experience section changed

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,9 +5,16 @@ import styles from "../styles/page.module.css";
 const Experience = (props) => {
   return (
     <div className={styles["new-experience-wrapper"]}>
-      <p>{props.exp.period}</p>
-      <p>{props.exp.designation}</p>
-      <p>{props.exp.company}</p>
+      <h3>{props.exp.designation}</h3>
+      <p className={styles['period']}>{props.exp.period}</p>
+      <ul>
+        Key KPIs:
+        {props.exp.description.map((kpi) => {
+          return <li key={Math.random()}>
+            {kpi}
+          </li>
+        })}
+      </ul>
     </div>
   );
 };
--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "../styles/page.module.css";
 import Experience from "./Experience";
+import { useScroll } from "motion/react";
 
 const experiences = [
   {
     period: "June 2023 - Present",
     designation: "Member Technical Staff",
     company: "Zoho Corporation",
+    description: [
+      'Designed a NoSQL mimiced Planner and Executor to run queries in NoSQL format in Postgres Distributed Environment',
+      'Refined execution plans and resource allocation strategies to achieve 50% faster query execution within a distributed environment',
+      'Achieved up to 50 % improvement in file transfer speeds by leveraging the SCP protocol for lightweight, compressed and high-speed data transmission.',
+      'Skills: C, Postgres, RDBMS, C++, Postgres Internals',
+    ]
   },
   {
     period: "January 2023 - May 2023",
@@ -17,14 +24,22 @@ const experiences = [
 ];
 
 const Experiences = () => {
+  const [selectedCompany, setSelectedCompany] = useState(0);
+  const zohoClasses = `${styles['company-btn']} ${selectedCompany === 0 && styles['selected-company']}`
+
   return (
     <div id="experience" className={styles["experience-wrapper"]}>
       <p className={styles['caption']}>Contributing at</p>
       <h2 className={styles["title"]}>Work</h2>
-      <div className={styles["experiences"]}>
-        {experiences.map((experience) => (
-          <Experience exp={experience} key={Math.random()} />
-        ))}
+      <div className={styles["experiences_wrapper"]}>
+        <div className={styles['experiences']}>
+          <ol className={styles["companies-list_wrapper"]}>
+            <li>
+              <button className={zohoClasses}>Zoho Corporation</button>
+            </li>
+          </ol>
+          <Experience exp={experiences[selectedCompany]}/>
+        </div>
       </div>
     </div>
   );
