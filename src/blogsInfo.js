d9a124edd42920ee17f9084b3a0e2a4e218e9a07
V N G Samba Siva Reddy Chinta <151616423+sambasivareddy-ch@users.noreply.github.com>
2025-06-19 02:17:42 +0000

    System Design Day: 01

--- src/blogsInfo.js
+++ src/blogsInfo.js
@@ -1,12 +1,12 @@
 const blogs = [
     {
-        'id': 'sample',
-        'slug': 'sample',
-        'title': 'Sample Blog',
-        'description': 'This is a sample blog description.',
-        'date': '2023-10-01',
-        'domains': ['javascript', 'react'],
-        'filename': 'sample.md',
+        'id': 'system-design-01',
+        'slug': 'system-design-01',
+        'title': 'Day 1: System Design - Introduction to System Design',
+        'description': 'First day of the system design series, covering the basics of system design.',
+        'date': 'June 19, 2025',
+        'domains': ['system-design'],
+        'filename': 'system-design-01.md',
     },
     {
         'id': 'sample-1',
